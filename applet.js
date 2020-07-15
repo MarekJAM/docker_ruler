2be0a1b1a0f9ddad62c5ea1c4d20a62f98d593da
Marek <marekash@gmail.com>
2020-07-15 23:30:54 +0200

    on click displays list of all containers

--- applet.js
+++ applet.js
@@ -2,10 +2,18 @@ const Applet = imports.ui.applet;
 const Lang = imports.lang;
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const MessageTray = imports.ui.messageTray;
 const PopupMenu = imports.ui.popupMenu;
 const UUID = 'docker@marekjam';
 
-function ConfirmDialog(){
+let Docker;
+if (typeof require !== 'undefined') {
+  Docker = require('./docker');
+} else {
+  Docker = imports.ui.appletManager.applets[UUID].docker;
+}
+
+function ConfirmDialog() {
   this._init();
 }
 
@@ -16,8 +24,12 @@ function MyApplet(orientation, panelHeight, instanceId) {
 MyApplet.prototype = {
   __proto__: Applet.IconApplet.prototype,
 
-  _init: function(orientation, panelHeight, instanceId) {
+  _init: function (orientation, panelHeight, instanceId) {
     Applet.IconApplet.prototype._init.call(this, orientation, panelHeight, instanceId);
+
+    this._msgsource = new MessageTray.SystemNotificationSource("Docker Ruler");
+    Main.messageTray.add(this._msgsource);
+
 
     try {
       this.set_applet_icon_name("inactive");
@@ -30,18 +42,36 @@ MyApplet.prototype = {
       this._contentSection = new PopupMenu.PopupMenuSection();
       this.menu.addMenuItem(this._contentSection);
 
-      // First item: Turn on
-      let item = new PopupMenu.PopupMenuItem("Container");
-      this.menu.addMenuItem(item);
+      this.docker = new Docker.Docker();
 
+      let containers = this.docker.listContainers();
+      if (containers.length == 0) {
+        this.menu.addMenuItem(new PopupMenu.PopupMenuItem(
+          'No containers found.',
+        ));
+      } else {
+        for (var i = 0; i < containers.length; i++) {
+          let item = new PopupMenu.PopupMenuItem(containers[i].image);
+          item.connect('activate', Lang.bind(this, function () {
+            //  Main.Util.spawnCommandLine("gnome-terminal -e 'sh -c \"echo "+test.names+"; sleep 10; exec bash\"\'");
+          }));
+          this.menu.addMenuItem(item);
+
+        }
+      }
     }
     catch (e) {
       global.logError(e);
     }
   },
 
-  on_applet_clicked: function(event) {
+  on_applet_clicked: function (event) {
     this.menu.toggle();
+  },
+
+  notification: function (message) {
+    let notification = new MessageTray.Notification(this._msgsource, "Docker Ruler", message);
+    this._msgsource.notify(notification);
   },
 };
 
@@ -49,3 +79,4 @@ function main(metadata, orientation, panelHeight, instanceId) {
   let myApplet = new MyApplet(orientation, panelHeight, instanceId);
   return myApplet;
 }
+
---
+++ docker.js
@@ -0,0 +1,59 @@
+/*jshint esversion: 6 */
+const Mainloop = imports.mainloop;
+const GLib = imports.gi.GLib;
+const Lang = imports.lang;
+const Main = imports.ui.main;
+
+
+function Docker() {
+    this._init();
+}
+
+Docker.prototype = {
+    _init: function() {
+    },
+
+    listContainers: function () {
+        return this.colCommand('ps -a');
+    },
+
+    parseColumnMetadata: function(original_row) {
+        let row = original_row;
+        let last_reduction = '';
+        while (row != last_reduction) {
+            last_reduction = row;
+            row = row.replace(/   /gi, '  ');
+        }
+        let positions = [];
+        row.split('  ').forEach(function(element) {
+            positions.push({
+                name: element,
+                position: original_row.match(element).index,
+                length: -1
+            });
+        });
+        for (let index = 0; index < positions.length - 1; index++) {
+            positions[index].length = positions[index + 1].position - positions[index].position;
+        }
+        return positions;
+    },
+
+    colCommand: function(command) {
+        let [res, list, err, status] = GLib.spawn_command_line_sync('docker' + ' ' + command);
+        let output_lines = list.toString().split('\n');
+        let columns = this.parseColumnMetadata(output_lines[0]);
+        let lines = [];
+        for (let index = 1; index < output_lines.length - 1; index++) {
+            let line = {};
+            for (let column_index = 0; column_index < columns.length; column_index++) {
+                if (columns[column_index].length != -1) {
+                    line[columns[column_index].name.toLowerCase()] = output_lines[index].substr(columns[column_index].position, columns[column_index].length).trim();
+                } else {
+                    line[columns[column_index].name.toLowerCase()] = output_lines[index].substr(columns[column_index].position).trim();
+                }
+            }
+            lines.push(line);
+        }
+        return lines;
+    },
+};
